e8efa34901798d8f98cc235066090446cab66a1b
itxbasit <109472859+itxbasit@users.noreply.github.com>
2023-01-27 23:57:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//     !!!!!!!   Chapter 1  !!!!!!!   
+// Question No 1
+
+// alert("Welcome, to JavaScript world!")
+
+// Question No 2
+
+// alert("JavaScript Alert\n\nError! Please enter a valid password")
+
+// Question No 3
+
+// alert("JavaScript Alert\n\nWelcome to JS Land...\nHappy Coding!")
+
+// Question No 4
+
+// alert("JavaScript Alert\n\nWelcome to JS Land...")
+// alert("JavaScript Alert\n\nHappy Coding!\nPrevent this page from creating additional dialogs.")
+
+// Question No 5
+// alert("JavaScript Alert\n\nHello... I can run JS through my web browser's console")
+
